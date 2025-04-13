50c1cbbccea7eb8c604ea068ebfb1069ae9c5022
Ostrometskii Dmitry <lightwalker@tutamail.com>
2025-04-13 18:39:04 +0300

    tr3

--- printmd.py
+++ printmd.py
@@ -1,4 +1,5 @@
 from IPython.display import Latex
+from IPython.display import display
 
 def mdMatrix(label, arr):
     str = f"$${label}" + "= \\begin{pmatrix}" + " \\\\ ".join([' & '.join(["{:.2f}".format(elem) for elem in line.astype(float)]) for line in arr]) + "\\end{pmatrix}$$"
@@ -7,3 +8,10 @@ def mdMatrix(label, arr):
 def mdVector(label, arr):
     str = f"$${label}" + "= \\begin{pmatrix}" + " \\\\ ".join(["{:.2f}".format(elem) for elem in arr]) + "\\end{pmatrix}$$"
     return  Latex(str)
+
+def mdLabelNumber(label, num, pres = 4):
+    str = f"$${label}" + f"= {num:10.{pres}f}$$"
+    return Latex(str)
+
+def printMd(str):
+    display(Latex(str))
